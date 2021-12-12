736360a75802d70cfd79347061e77df8480b71cc
mygoaway2 <mygoaway2@gmail.com>
2021-12-12 20:24:35 +0900

    first commit

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,27 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_leve2() {
+        TraceStatus status1 = trace.begin("HELLO1");
+        TraceStatus status2 = trace.begin("HELLO2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_leve2() {
+        TraceStatus status1 = trace.begin("HELLO1");
+        TraceStatus status2 = trace.begin("HELLO2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
