4db413850753407176a227e191767a212c681a40
mygoaway2 <mygoaway2@gmail.com>
2021-12-12 17:35:54 +0900

    Initial commit

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,27 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    @DisplayName("begin_end")
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    @DisplayName("begin_exception")
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV2Test {
+
+    @Test
+    @DisplayName("begin_end")
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    @DisplayName("begin_exception")
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
